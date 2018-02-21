19a071d2a449970c91d92644c322132dc18a22f6
Jan Calanog <jan.calanog@iteratec.at>
2018-02-21 17:37:15 +0100

    added extract css + minify

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
 var path = require('path');
-var elmSource = __dirname + '/elm/path/in/project'
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const uglifyjs = new UglifyJsPlugin({
+  sourceMap: true,
+  uglifyOptions: {
+    output: { comments: false }
+  }
+});
+
+const extractText = new ExtractTextPlugin('style.css');
 
 module.exports = {
+  plugins: [ uglifyjs, extractText ],
   entry: {
     app: [
       './src/index.js'
     ]
   },
-
   output: {
     path: path.resolve(__dirname + '/dist'),
     filename: '[name].js',
@@ -16,11 +26,27 @@ module.exports = {
     rules: [
       {
         test: /\.(css|scss|sass)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+                loader: 'css-loader',
+                options: {
+                    // If you are having trouble with urls not resolving add this setting.
+                    // See https://github.com/webpack-contrib/css-loader#url
+                    url: false,
+                    minimize: true,
+                    sourceMap: true
+                }
+            }, 
+            {
+                loader: 'sass-loader',
+                options: {
+                    sourceMap: true
+                }
+            }
+          ]
+        })
       },
       {
         test: /\.jsx?$/,
