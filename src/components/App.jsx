e766b451b1bcd88041a58d1dfd7fb04f338d45f7
Jan Calanog <jan.calanog@iteratec.at>
2018-03-19 14:47:03 +0100

    Added github link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import DishesEditorWrapper from './DishesEditorWrapper.jsx';
 const App = () => (
-    <DishesEditorWrapper />
+    <div>
+        <div style={{padding: '1rem 0 0 0'}}>
+            <nav className="level">
+                <p className="level-item has-text-centered">
+                    <a className="button is-small" target="_blank" rel="noopener noreferrer" href="https://github.com/reakAleek/react-without-redux-example-application">
+                        <span className="icon">
+                        <i className="fa fa-github"></i>
+                        </span>
+                        <span>GitHub</span>
+                    </a>
+                </p> 
+            </nav>
+        </div>
+        <DishesEditorWrapper />
+    </div>
+    
 );
 
 export default App;
